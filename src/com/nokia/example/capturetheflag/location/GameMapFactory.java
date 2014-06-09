41560418cd6a9978eeeec7e7adcbaef62db018ce
Lauri Taipale <lauri.taipale@octo3.fi>
2014-06-09 10:46:18 +0300

    Using reflection in factories to avoid runtime warnings for unresolved super classes.

--- src/com/nokia/example/capturetheflag/location/GameMapFactory.java
+++ src/com/nokia/example/capturetheflag/location/GameMapFactory.java
@@ -5,17 +5,16 @@
 
 package com.nokia.example.capturetheflag.location;
 
-import com.nokia.example.capturetheflag.location.google.GameMapGoogle;
-import com.nokia.example.capturetheflag.location.here.GameMapHere;
-
-import android.util.Log;
 
 /**
  * Factory class for instantiating either Here or Google Maps UI map fragment.
  * @see GameMapInterface.
  */
 public class GameMapFactory {
+	
     private static final String HERE_MAP_CLASS_NAME = "com.here.android.mapping.Map";
+    private static final String HERE_GAME_MAP_CLASS_NAME = "com.nokia.example.capturetheflag.location.here.GameMapHere";
+    private static final String GOOGLE_GAME_MAP_CLASS_NAME = "com.nokia.example.capturetheflag.location.google.GameMapGoogle";    
     
     private static final String TAG = "CtF/MapFactory";
 
@@ -39,13 +38,15 @@ public class GameMapFactory {
      */
     public static GameMapInterface createGameMap() {
         GameMapInterface map = null;
-        if(isHereMapsAvailable()) {
-            Log.d(TAG, "Using Here Maps");
-            map = new GameMapHere();
-        } else {
-            Log.d(TAG, "Using Google Maps");
-            map = new GameMapGoogle();
-        }
+        String className = isHereMapsAvailable() ? HERE_GAME_MAP_CLASS_NAME : GOOGLE_GAME_MAP_CLASS_NAME;
+        
+        try {
+			map = (GameMapInterface)Class.forName(className).newInstance();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
         return map;
     }
 }
--- src/com/nokia/example/capturetheflag/location/LocationManagerFactory.java
+++ src/com/nokia/example/capturetheflag/location/LocationManagerFactory.java
@@ -5,11 +5,9 @@
 
 package com.nokia.example.capturetheflag.location;
 
-import com.nokia.example.capturetheflag.location.google.LocationManagerGoogle;
-import com.nokia.example.capturetheflag.location.here.LocationManagerHere;
+import java.lang.reflect.Constructor;
 
 import android.app.Activity;
-import android.util.Log;
 
 /**
  * Factory class for instantiating either Here or Google specific implementation
@@ -20,6 +18,8 @@ import android.util.Log;
  */
 public class LocationManagerFactory {
     private static final String HERE_POSITIONING_CLASS_NAME = "com.here.android.common.PositioningManager";
+	private static final String HERE_LOCATION_CLASS_NAME = "com.nokia.example.capturetheflag.location.here.LocationManagerHere";
+	private static final String GOOGLE_LOCATION_CLASS_NAME = "com.nokia.example.capturetheflag.location.google.LocationManagerGoogle";
 
     private static final String TAG = "CtF/LocationManagerFactory";
 
@@ -43,13 +43,16 @@ public class LocationManagerFactory {
     public static LocationManagerInterface getLocationManagerInterface(
             Activity activity) {
         if (mInstance == null) {
-            if (isHerePositioningAvailable()) {
-                Log.d(TAG, "Using Here for positioning");
-                mInstance = new LocationManagerHere(activity);
-            } else {
-                Log.d(TAG, "Using Google for positioning");
-                mInstance = new LocationManagerGoogle(activity);
-            }
+        	
+            String className = isHerePositioningAvailable() ? HERE_LOCATION_CLASS_NAME : GOOGLE_LOCATION_CLASS_NAME;
+            
+            try {
+                Constructor<?> constructor = Class.forName(className).getConstructor(Activity.class);
+                mInstance = (LocationManagerInterface)constructor.newInstance(activity);
+    		} catch (Exception e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
         }
 
         return mInstance;
