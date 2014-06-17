d1f1baef647f9270071c76a59ff2394474f09c19
Lauri Taipale <lauri.taipale@octo3.fi>
2014-06-17 11:48:40 +0300

    Minor cleanup, refactoring and commenting of PurchasePremiumFragment class. Removed unused and deprecated PremiumHandler class. Added first draft of IAP documentation and added one space to strings.

--- capture-the-flag/src/com/nokia/example/capturetheflag/PurchasePremiumFragment.java
+++ capture-the-flag/src/com/nokia/example/capturetheflag/PurchasePremiumFragment.java
@@ -5,18 +5,6 @@
 
 package com.nokia.example.capturetheflag;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.onepf.oms.OpenIabHelper;
-import org.onepf.oms.appstore.googleUtils.IabHelper;
-import org.onepf.oms.appstore.googleUtils.IabResult;
-import org.onepf.oms.appstore.googleUtils.Inventory;
-import org.onepf.oms.appstore.googleUtils.Purchase;
-import org.onepf.oms.appstore.googleUtils.SkuDetails;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Fragment;
@@ -32,25 +20,41 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.onepf.oms.OpenIabHelper;
+import org.onepf.oms.appstore.googleUtils.IabHelper;
+import org.onepf.oms.appstore.googleUtils.IabResult;
+import org.onepf.oms.appstore.googleUtils.Inventory;
+import org.onepf.oms.appstore.googleUtils.Purchase;
+import org.onepf.oms.appstore.googleUtils.SkuDetails;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * A fragment for upgrading the app to premium version.
- * Uses the OpenIAB library to request product info and
- * purchasing premium version.
+ *
+ * Uses the OpenIAB library to request product info and purchasing premium version.
+ * @see <a href="https://github.com/onepf/OpenIAB">OpenIAB documentation</a>.
  */
 public class PurchasePremiumFragment extends Fragment implements MainActivity.BackCallback {
 
     private static final String TAG = "CtF/PurchasePremiumFragment";
+
     public static final String FRAGMENT_TAG = "PurchasePremiumFragment";
     public static final int RC_REQUEST = 10001;
 
     // Nokia In-App Payment constants
-    private static final String ITEM_TYPE_INAPP = "inapp";
     private static final String NOKIA_PREMIUM_PRODUCT_ID = "1023610"; // Test ID
 
     // Google In-App Billing constants
     private static final String Base64EncodedPublicKey = "INSERT_YOUR_PUBLIC_KEY_HERE";
     private static final String SKU_PREMIUM = "android.test.purchased";
 
+    /**
+     * SKU mappings
+     */
     static {
         OpenIabHelper.mapSku(SKU_PREMIUM, OpenIabHelper.NAME_NOKIA, NOKIA_PREMIUM_PRODUCT_ID);
     }
@@ -66,6 +70,94 @@ public class PurchasePremiumFragment extends Fragment implements MainActivity.Ba
     private Button mBuyButton;
 
     private ProgressDialog mWaitScreen;
+
+    /**
+     * Callback for when setup is finished. See OpenIAB documentation for details.
+     */
+    private IabHelper.OnIabSetupFinishedListener mSetupListener = new IabHelper.OnIabSetupFinishedListener() {
+        @Override
+        public void onIabSetupFinished(IabResult result) {
+            if (!result.isSuccess()) {
+                toast("Problem setting up in-app billing: " + result);
+                Log.e(TAG, "Problem setting up in-app billing: " + result);
+            } else {
+                mSetupDone = true;
+                Log.d(TAG, "Setup successful. Querying inventory.");
+
+                mWaitScreen = ProgressDialog.show(getActivity(), null, getText(R.string.fetching_product_info));
+
+                // Query for already purchased items and SKU details for items what user can buy.
+                List<String> skus = new ArrayList<String>();
+                skus.add(SKU_PREMIUM);
+                mHelper.queryInventoryAsync(true, skus, mGotInventoryListener);
+            }
+        }
+    };
+
+    /**
+     * Callback for when inventory query is finished. See OpenIAB documentation for details.
+     */
+    private IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+        public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
+            Log.d(TAG, "Query inventory finished.");
+
+            if (result.isFailure()) {
+                Log.d(TAG, "Failed to query inventory: " + result);
+
+                toast("Failed to query inventory: " + result);
+                mTitleLine.setText("Failed to fetch product info");
+            } else {
+                Log.d(TAG, "Query inventory was successful.");
+
+                SkuDetails premiumDetails = inventory.getSkuDetails(SKU_PREMIUM);
+                if (premiumDetails != null) {
+                    String title = premiumDetails.getTitle();
+                    String description = premiumDetails.getDescription();
+                    String price = premiumDetails.getPrice();
+
+                    mTitleLine.setText(title);
+                    mDescriptionLine.setText(description);
+                    mPriceLine.setText("Upgrade to premium (" + price + ") (user not charged!)");
+                } else {
+                    mTitleLine.setText("Failed to fetch product info");
+                }
+
+                // Do we already have the premium upgrade purchased?
+                Purchase premiumPurchase = inventory.getPurchase(SKU_PREMIUM);
+                mIsPremium = (premiumPurchase != null && verifyDeveloperPayload(premiumPurchase));
+                Log.d(TAG, "User is " + (mIsPremium ? "PREMIUM" : "NOT PREMIUM"));
+
+                if (mIsPremium) {
+                    purchased();
+                }
+            }
+            dismissWaitScreen();
+        }
+    };
+
+    /**
+     * Callback for when a purchase is finished. See OpenIAB documentation for details.
+     */
+    private IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
+            Log.d(TAG, "Purchase finished: " + result + ", purchase: " + purchase);
+            if (result.isFailure()) {
+                Log.d(TAG, "Error purchasing: " + result);
+                alert("Error purchasing: " + result);
+            } else if (!verifyDeveloperPayload(purchase)) {
+                alert("Error purchasing. Authenticity verification failed.");
+            } else {
+                Log.d(TAG, "Purchase successful.");
+
+                if (purchase.getSku().equals(SKU_PREMIUM)) {
+                    toast("Thank you for upgrading to premium!");
+                    mIsPremium = true;
+                    purchased();
+                }
+            }
+            dismissWaitScreen();
+        }
+    };
 
     @Override
     public void onAttach(Activity activity) {
@@ -93,161 +185,17 @@ public class PurchasePremiumFragment extends Fragment implements MainActivity.Ba
 
         if (Base64EncodedPublicKey.contains("INSERT_YOUR")) {
             mBuyButton.setEnabled(false);
-            alert("Please put your app's public key in PurchasePremiumFragment.java. See README.");
+            alert("Please put your app's public key in PurchasePremiumFragment.java. See documentation.");
         } else {
             Map<String, String> storeKeys = new HashMap<String, String>();
             storeKeys.put(OpenIabHelper.NAME_GOOGLE, Base64EncodedPublicKey);
 
             mHelper = new OpenIabHelper(getActivity(), storeKeys);
-            mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
-                public void onIabSetupFinished(IabResult result) {
-                    if (!result.isSuccess()) {
-                        toast("Problem setting up in-app billing: " + result);
-                        Log.e(TAG, "Problem setting up in-app billing: " + result);
-                        return;
-                    }
-
-                    mSetupDone = true;
-                    Log.d(TAG, "Setup successful. Querying inventory.");
-
-                    mWaitScreen = ProgressDialog.show(getActivity(), null, getText(R.string.fetching_product_info));
-
-                    //Query for already purchased items and 
-                    //SKU details for items what user can buy
-                    List<String> skus = new ArrayList<String>();
-                    skus.add(SKU_PREMIUM);
-                    mHelper.queryInventoryAsync(true, skus, mGotInventoryListener);
-                }
-            });
+            mHelper.startSetup(mSetupListener);
         }
 
         return v;
     }
-
-    IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
-        public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
-            Log.d(TAG, "Query inventory finished.");
-            if (result.isFailure()) {
-                toast("Failed to query inventory: " + result);
-                Log.d(TAG, "Failed to query inventory: " + result);
-                mTitleLine.setText("Failed to fetch product info");
-                dismissWaitScreen();
-                return;
-            }
-
-            SkuDetails premiumDetails = inventory.getSkuDetails(SKU_PREMIUM);
-
-            if (premiumDetails != null) {
-                String title = premiumDetails.getTitle();
-                String description = premiumDetails.getDescription();
-                String price = premiumDetails.getPrice();
-
-                mTitleLine.setText(title);
-                mDescriptionLine.setText(description);
-                mPriceLine.setText("Upgrade to premium (" + price + ") (user not charged!)");
-            } else {
-                mTitleLine.setText("Failed to fetch product info");
-            }
-
-            Log.d(TAG, "Query inventory was successful.");
-
-            // Do we have the premium upgrade?
-            Purchase premiumPurchase = inventory.getPurchase(SKU_PREMIUM);
-            mIsPremium = (premiumPurchase != null && verifyDeveloperPayload(premiumPurchase));
-            Log.d(TAG, "User is " + (mIsPremium ? "PREMIUM" : "NOT PREMIUM"));
-
-            if (mIsPremium) {
-                purchased();
-            }
-
-            dismissWaitScreen();
-        }
-    };
-
-    private void dismissWaitScreen() {
-        mWaitScreen.dismiss();
-        mWaitScreen = null;
-    }
-
-    private void purchased() {
-        mBuyButton.setText("Purchased");
-        mBuyButton.setEnabled(false);
-        Settings.setPremium(SKU_PREMIUM, getActivity());
-        ((MainActivity) getActivity()).unlockPremium();
-    }
-
-    boolean verifyDeveloperPayload(Purchase p) {
-        /*
-         * WARNING: Locally generating a random string when starting a purchase and 
-         * verifying it here might seem like a good approach, but this will fail in the 
-         * case where the user purchases an item on one device and then uses your app on 
-         * a different device, because on the other device you will not have access to the
-         * random string you originally generated.
-         *
-         * So a good developer payload has these characteristics:
-         * 
-         * 1. If two different users purchase an item, the payload is different between them,
-         *    so that one user's purchase can't be replayed to another user.
-         * 
-         * 2. The payload must be such that you can verify it even when the app wasn't the
-         *    one who initiated the purchase flow (so that items purchased by the user on 
-         *    one device work on other devices owned by the user).
-         * 
-         * Using your own server to store and verify developer payloads across app
-         * installations is recommended.
-         */
-
-        String payload = p.getDeveloperPayload();
-
-        if (payload.equals(mDeveloperPayload)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public void purchasePremium() {
-        if (!mSetupDone) {
-            toast("Billing setup is not completed yet, try again after a while.");
-            return;
-        }
-
-        if (!mIsPremium) {
-            mWaitScreen = ProgressDialog.show(getActivity(), null, getText(R.string.purchasing_product));
-            mHelper.launchPurchaseFlow(getActivity(), SKU_PREMIUM, RC_REQUEST, mPurchaseFinishedListener, mDeveloperPayload);
-        } else {
-            toast("Premium already purchased.");
-            mBuyButton.setEnabled(false);
-        }
-    }
-
-    // Callback for when a purchase is finished
-    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
-        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
-            Log.d(TAG, "Purchase finished: " + result + ", purchase: " + purchase);
-            if (result.isFailure()) {
-                Log.d(TAG, "Error purchasing: " + result);
-                alert("Error purchasing: " + result);
-                dismissWaitScreen();
-                return;
-            }
-
-            if (!verifyDeveloperPayload(purchase)) {
-                alert("Error purchasing. Authenticity verification failed.");
-                dismissWaitScreen();
-                return;
-            }
-
-            Log.d(TAG, "Purchase successful.");
-            dismissWaitScreen();
-
-            if (purchase.getSku().equals(SKU_PREMIUM)) {
-                toast("Thank you for upgrading to premium!");
-                mIsPremium = true;
-                purchased();
-            }
-        }
-    };
 
     @Override
     public void onDestroy() {
@@ -259,24 +207,107 @@ public class PurchasePremiumFragment extends Fragment implements MainActivity.Ba
         }
     }
 
+    @Override
+    public void onBackPressed() {
+        getFragmentManager().beginTransaction().remove(this).commit();
+        ((MainActivity) getActivity()).setBackCallback(null);
+    }
+
+    /**
+     * Dismisses the wait screen.
+     */
+    private void dismissWaitScreen() {
+        mWaitScreen.dismiss();
+        mWaitScreen = null;
+    }
+
+    /**
+     * Updates the UI and unlocks the premium functionality when the purchase has been completed.
+     */
+    private void purchased() {
+        mBuyButton.setText("Purchased");
+        mBuyButton.setEnabled(false);
+        Settings.setPremium(SKU_PREMIUM, getActivity());
+        ((MainActivity) getActivity()).unlockPremium();
+    }
+
+    /**
+     * Verifies the authenticity of the purchase.
+     *
+     * WARNING: Locally generating a random string when starting a purchase and
+     * verifying it here might seem like a good approach, but this will fail in the
+     * case where the user purchases an item on one device and then uses your app on
+     * a different device, because on the other device you will not have access to the
+     * random string you originally generated.
+     *
+     * So a good developer payload has these characteristics:
+     *
+     * 1. If two different users purchase an item, the payload is different between them,
+     *    so that one user's purchase can't be replayed to another user.
+     *
+     * 2. The payload must be such that you can verify it even when the app wasn't the
+     *    one who initiated the purchase flow (so that items purchased by the user on
+     *    one device work on other devices owned by the user).
+     *
+     * Using your own server to store and verify developer payloads across app
+     * installations is recommended.
+     *
+     * @param purchase The {@link org.onepf.oms.appstore.googleUtils.Purchase} to verify.
+     * @return <code>true</code> if the payload was valid, <code>false</code> if not.
+     */
+    private boolean verifyDeveloperPayload(Purchase purchase) {
+        String payload = purchase.getDeveloperPayload();
+
+        return payload.equals(mDeveloperPayload);
+    }
+
+    /**
+     * Initiates the purchase process for the premium product if not already purchased.
+     */
+    public void purchasePremium() {
+        if (!mSetupDone) {
+            toast("Billing setup is not completed yet, try again after a while.");
+        } else {
+            if (!mIsPremium) {
+                mWaitScreen = ProgressDialog.show(getActivity(), null, getText(R.string.purchasing_product));
+                mHelper.launchPurchaseFlow(getActivity(), SKU_PREMIUM, RC_REQUEST, mPurchaseFinishedListener, mDeveloperPayload);
+            } else {
+                toast("Premium already purchased.");
+                mBuyButton.setEnabled(false);
+            }
+        }
+    }
+
+    /**
+     * Handles an in-app purchase activity result with request code = PurchasePremiumFragment.RC_REQUEST.
+     * Called from {@link com.nokia.example.capturetheflag.MainActivity#onActivityResult(int, int, android.content.Intent)}.
+     *
+     * @param requestCode The request code.
+     * @param resultCode  The result code.
+     * @param data The data.
+     */
     public void handleActivityResult(int requestCode, int resultCode, Intent data) {
         mHelper.handleActivityResult(requestCode, resultCode, data);
     }
 
+    /**
+     * Creates and shows toast with the given text.
+     *
+     * @param message Text for the toast.
+     */
     private void toast(String message) {
         Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
     }
 
+    /**
+     * Creates and shows an alert dialog with the given text.
+     *
+     * @param message Text for the alert dialog.
+     */
     private void alert(String message) {
         AlertDialog.Builder bld = new AlertDialog.Builder(getActivity());
         bld.setMessage(message);
         bld.setNeutralButton("OK", null);
         bld.create().show();
-    }
-
-    @Override
-    public void onBackPressed() {
-        getFragmentManager().beginTransaction().remove(this).commit();
-        ((MainActivity) getActivity()).setBackCallback(null);
     }
 }
---
+++ doc/iap.md
@@ -0,0 +1,27 @@
+## In-App Purchase
+
+**NOTE**: The SKU product identifiers used in the example application are for test products and should be replaced with real products ids. Also the Google In-App Billing public key needs to be replaced with a valid key for the in-app purchase to work.
+
+__<< TODO: Insert class diagram >>__
+
+### Implementation
+
+The in-app purchase functionality of the example application demonstrates how purchasing a "premium version" of the application with unlimited functionality is implemented using functionality provided by the [OpenIAB](https://github.com/onepf/OpenIAB) library. The OpenIAB library is a wrapper component that encapsulates the use of different in-app purchase APIs, in this case the **Nokia IAP** and **Google Play**, behind a single easy-to-use API.
+
+All the implementation related the in-app purchase functionality is implemented in the `PurchasePremiumFragment` class of the example application. The class provides the UI Fragment for the purchasing view and uses the IAB library's APIs to perform all the parts of the in-app purchase process.
+
+1. The OpenIAB component set up is started asynchronously when the PurchasePremiumFragment is created by calling `OpenIabHelper.startSetup(...)`.
+    
+2. Once the OpenIAB component setup is complete, the given `OnIabSetupFinishedListener`'s `onIabSetupFinished(IabResult result)` method is called. If the setup was successful the listener (`mSetupListener`) requests the list of available and already purchased products by calling `OpenIabHelper.queryInventoryAsync(...)`.
+
+3. When then inventory information is received the given `QueryInventoryFinishedListener`'s (`mGotInventoryListener`) `onQueryInventoryFinished(...)` method is called with the inventory information. If the "premium" product is found from the list of purchasable products, the product information and price are updated to the UI. Also, if the "premium" product is found from the list of already purchased items, the feature will then be unlocked.
+
+4. If the user chooses to purchase the "premium" product by pressing the "Upgrade" button the purchase flow is started simply by calling the 
+`OpenIabHelper.launchPurchaseFlow(...)` method.
+
+5. Once the in-app purchase flow is complete the given `OnIabPurchaseFinishedListener`'s (`mPurchaseFinishedListener`) onIabPurchaseFinished method is called with the purchase result and information. If the purchase was successful and the purchase payload verified, the "premium" feature will be marked as purchased and the related functionality unlocked.
+
+### Links
+
+- [OpenIAB on GitHub](https://github.com/onepf/OpenIAB)
+- [Nokia In-App Payment documentation on developer.nokia.com](http://developer.nokia.com/resources/library/nokia-x/nokia-in-app-payment.html)
